b09e0eaf0707940350c2245d999ebb989adcb7cc
dievskiy <potekhin.ruslan@gmail.com>
2022-05-21 19:25:37 +0300

    Exercise 1.05: Project v0.3

---
+++ 103/app.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+import datetime
+import os
+import uuid
+
+from flask import (Flask, render_template)
+
+port = os.environ['PORT']
+app = Flask(__name__)
+
+
+def generate_random_string() -> str:
+    """
+    Generates random string
+    """
+    return str(uuid.uuid4())
+
+
+def print_port():
+    print(f'Server started in port {port}')
+
+
+@app.route('/', methods=['GET'])
+def main():
+    return render_template('index.html',
+                           current_time=str(datetime.datetime.now()),
+                           initial_string=initial_string
+                           )
+
+
+initial_string = generate_random_string()
+print_port()
