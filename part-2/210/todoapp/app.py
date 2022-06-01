97f714664ef0333fb9f2b258104c454f0b877e53
dievskiy <potekhin.ruslan@gmail.com>
2022-06-01 13:53:01 +0300

    Exercise 2.10: Project v1.3

---
+++ part-2/210/todoapp/app.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3.10
+
+import os
+from flask import Flask, request, jsonify
+from storage import save_todo, fetch_todos
+
+port = os.getenv('PORT', 4444)
+app = Flask(__name__)
+
+
+def is_valid(item: str) -> bool:
+    return 0 < len(item) < 140
+
+
+@app.route('/todos', methods=['GET', 'POST'])
+def get_todos():
+    if request.method == 'POST':
+        body = request.json
+        new_todo = body['item']
+        if not is_valid(new_todo):
+            print('Todo is invalid, longer than 140 symbols')
+            return jsonify({'message': 'Todo content is too long'}), 400
+        save_todo(new_todo)
+        return '', 204
+    else:
+        todos = fetch_todos()
+        return f'{todos}'
